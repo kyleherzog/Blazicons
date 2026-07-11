ec7ab834370ad626b4b9554ccae32ee206e55293
Kyle Herzog <holler@kyleherzog.com>
2026-07-11 15:35:05 -0500

    file reorgs

--- Blazicons/BlaziconAnimationDirection.cs
+++ Blazicons/BlaziconAnimationDirection.cs
@@ -25,17 +25,3 @@ public enum BlaziconAnimationDirection
     /// </summary>
     AlternateReverse,
 }
-
-internal static class BlaziconAnimationDirectionExtensions
-{
-    internal static string ToCssValue(this BlaziconAnimationDirection direction)
-    {
-        return direction switch
-        {
-            BlaziconAnimationDirection.Reverse => "reverse",
-            BlaziconAnimationDirection.Alternate => "alternate",
-            BlaziconAnimationDirection.AlternateReverse => "alternate-reverse",
-            _ => "normal",
-        };
-    }
-}
---
+++ Blazicons/Extensions/BlaziconAnimationDirectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazicons.Extensions;
+
+internal static class BlaziconAnimationDirectionExtensions
+{
+    internal static string ToCssValue(this BlaziconAnimationDirection direction)
+    {
+        return direction switch
+        {
+            BlaziconAnimationDirection.Reverse => "reverse",
+            BlaziconAnimationDirection.Alternate => "alternate",
+            BlaziconAnimationDirection.AlternateReverse => "alternate-reverse",
+            _ => "normal",
+        };
+    }
+}
